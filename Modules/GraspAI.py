a6d2396a48d40db255fb2bfe494b45ef1115ea00
Alex de Andrade <arexandrade@gmail.com>
2024-03-14 18:18:18 -0300

    GRASP AI type error correction

--- Modules/GraspAI.py
+++ Modules/GraspAI.py
@@ -24,7 +24,7 @@ class GraspAI:
     def generate_random_values(self, resources):
         new_values = self.current_values
         for idx, resource in enumerate(resources.items()):
-            new_values[idx] = randint(resource[1]['minimum_value'], resource[1]['maximum_value'])
+            new_values[idx] = randint(int(resource[1]['minimum_value']), int(resource[1]['maximum_value']))
 
         return new_values
 
